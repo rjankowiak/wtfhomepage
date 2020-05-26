6957a86d354efaae97b211902f3aed497cbdb8e8
Rafał Jankowiak <rjankowiak@intellihouse.pl>
2020-05-26 07:28:07 +0200

    hambuger menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-function greetings(name){
-    const greetText = `Cześć ${name}!`;
-    setText('.header__greetings', greetText);
-}
+// function greetings(name){
+//     const greetText = `Cześć ${name}!`;
+//     setText('.header__greetings', greetText);
+// }
 
-function setText(selector, text){
-    console.log(text);
-    const box = document.querySelector(selector);
-    box.innerHTML = text;
-}
+// function setText(selector, text){
+//     console.log(text);
+//     const box = document.querySelector(selector);
+//     box.innerHTML = text;
+// }
 
 //var name = prompt("Podaj imię:");
 //greetings(name);
-setText('.header__title', 'Witam na mojej stronie podróżniku!');
+//setText('.header__title', 'Witam na mojej stronie podróżniku!');
+
+const burgerButton = document.querySelector('.menu__button');
+burgerButton.addEventListener('click', () => {
+    const menu = document.querySelector('.menu__group');
+    menu.classList.toggle('menu__group--show');
+})
